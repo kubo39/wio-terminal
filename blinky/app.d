6cfe6921227148a7f91470676d270cf6e1f8397c
Hiroki Noda <kubo39@gmail.com>
2025-07-05 07:44:20 +0900

    add GpioPin and Led struct, for abstraction layer

--- blinky/app.d
+++ blinky/app.d
@@ -9,45 +9,105 @@ nothrow:
 __gshared @section(".isr_vector._reset") typeof(&resetHandler) _reset = &resetHandler;
 
 /* register addresses */
-__gshared
+private __gshared
 {
-    const uint PORT_ADDRESS_BASE = 0x4100_8000;
-    const uint PA_ADDRESS_BASE = PORT_ADDRESS_BASE + 0x80 * 0;
-    const uint PC_ADDRESS_BASE = PORT_ADDRESS_BASE + 0x80 * 2;
-    const uint PA_DIRSET_BASE = PA_ADDRESS_BASE + 0x08;
-    const uint PA_OUTSET_BASE = PA_ADDRESS_BASE + 0x14;
-    const uint PA_OUTCLR_BASE = PA_ADDRESS_BASE + 0x18;
-    const uint PC_DIRCLR_BASE = PC_ADDRESS_BASE + 0x04;
-    const uint PC_PINCFG26_BASE = PC_ADDRESS_BASE + 0x40 + 26;
-    const uint PC_IN_BASE = PC_ADDRESS_BASE + 0x20;
+    const uint PORT_ADDRESS = 0x4100_8000;
+    const uint PA_ADDRESS = PORT_ADDRESS + 0x80 * 0;
+    const uint PC_ADDRESS = PORT_ADDRESS + 0x80 * 2;
+    const uint PA_DIRSET = PA_ADDRESS + 0x08;
+    const uint PA_OUTSET = PA_ADDRESS + 0x14;
+    const uint PA_OUTCLR = PA_ADDRESS + 0x18;
+    const uint PC_DIRCLR = PC_ADDRESS + 0x04;
+    const uint PC_PINCFG26 = PC_ADDRESS + 0x40 + 26;
+    const uint PC_IN = PC_ADDRESS + 0x20;
 }
 
 enum uint PA_BIT_LED = 15;
 enum uint PC_BIT_BUTTON1 = 26;
 
+private struct Uninitialized {}
+private struct PushPullOutput {}
+
+struct GpioA15Pin(T)
+{
+    @disable this(this);
+
+    static if (is(T == Uninitialized))
+    {
+        GpioA15Pin!(PushPullOutput)* pushPullOutput()
+        {
+            // Set pin configuration
+            *(cast(uint*) PA_DIRSET) = 1 << PA_BIT_LED;
+            __gshared static GpioA15Pin!PushPullOutput pin;
+            pin = GpioA15Pin!PushPullOutput();
+            return &pin;
+        }
+    }
+
+    static if (is(T == PushPullOutput))
+    {
+        void setHigh()
+        {
+            *(cast(uint*) PA_OUTSET) = 1 << PA_BIT_LED;
+        }
+
+        void setLow()
+        {
+            *(cast(uint*) PA_OUTCLR) = 1 << PA_BIT_LED;
+        }
+    }
+}
+
+GpioA15Pin!Uninitialized* getGpioA15Pin()
+{
+    __gshared static GpioA15Pin!Uninitialized uninitPin;
+    uninitPin = GpioA15Pin!Uninitialized();
+    return &uninitPin;
+}
+
+struct Led
+{
+private:
+    GpioA15Pin!PushPullOutput* pin;
+
+    @disable this(this);
+
+public:
+    this(GpioA15Pin!Uninitialized* pin) @nogc nothrow
+    {
+        this.pin = pin.pushPullOutput();
+    }
+
+    void turnOn() @nogc nothrow
+    {
+        this.pin.setHigh();
+    }
+
+    void turnOff() @nogc nothrow
+    {
+        this.pin.setLow();
+    }
+}
+
 noreturn main()
 {
     // Set pin configuration
-    auto PA_DIRSET = cast(uint*) PA_DIRSET_BASE;
-    *PA_DIRSET = 1 << PA_BIT_LED;
-    auto PC_DIRCLR = cast(uint*) PC_DIRCLR_BASE;
-    *PC_DIRCLR = 1 << PC_BIT_BUTTON1;
-    auto PC_PINCFG26 = cast(uint*) PC_PINCFG26_BASE;
-    *PC_PINCFG26 |= 1 << 1;
+    *(cast(uint*) PC_DIRCLR) = 1 << PC_BIT_BUTTON1;
+    *(cast(uint*) PC_PINCFG26) |= 1 << 1;
 
-    auto PC_IN = cast(uint*) PC_IN_BASE;
-    auto PA_OUTSET = cast(uint*) PA_OUTSET_BASE;
-    auto PA_OUTCLR = cast(uint*) PA_OUTCLR_BASE;
+    auto pin = getGpioA15Pin();
+    auto led = Led(pin);
+
     while (true)
     {
-        const button1Input = *PC_IN & (1 << PC_BIT_BUTTON1);
+        const button1Input = *(cast(uint*)PC_IN) & (1 << PC_BIT_BUTTON1);
         if (button1Input > 0)
         {
-            *PA_OUTSET = 1 << PA_BIT_LED;
+            led.turnOn();
         }
         else
         {
-            *PA_OUTCLR = 1 << PA_BIT_LED;
+            led.turnOff();
         }
     }
 }
